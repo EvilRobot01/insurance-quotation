051ef5543563337907cf15f9452cb1dc2f6873eb
Harold DRF <95robots@gmail.com>
2019-02-19 20:50:17 -0700

    print results on html

--- js/app.js
+++ js/app.js
@@ -1,131 +1,204 @@
-//Variables
+// Variables
 const form = document.getElementById('request-quote');
 
-//Event listeners
-addEventListeners();
-function addEventListeners(){
-document.addEventListener('DOMContentLoaded', function(){
 const html = new HTMLUI();
-html.displayYears();
-});
 
-//Submitted Form
-form.addEventListener('submit', function(e){
-    e.preventDefault();
 
-    //Read values from the form
-    const make = document.getElementById('make').value;
-    const year = document.getElementById('year').value;
-    //Read the radio buttons
-    const level = document.querySelector('input[name="level"]:checked').value;
 
-    //Check empty fields
-    if(make === '' || year === '' || level ===''){
-        html.displayError('No empty fields');
-    }else{
-        //Make the quotation
-        const insurance = new Insurance(make, year, level);
-        const price = insurance.calculateQuotation(insurance);
+// Event Listeners
+eventListeners();
 
-    }
-});
+function eventListeners() {
+     document.addEventListener('DOMContentLoaded', function() {
+          // Create the <option> for the years
+          
+          html.displayYears();
+     });
+     
+     // When the form is submitted
+     form.addEventListener('submit', function(e) {
+          e.preventDefault();
+          
+          // Read the values from the FORM
+          const make = document.getElementById('make').value;
+          const year = document.getElementById('year').value;
+          // Read the radio buttons
+          const level = document.querySelector('input[name="level"]:checked').value;
+
+          // Check that all the fields have something
+          if( make === '' || year === '' || level === '' ) {
+               html.displayError('All the fields are mandatory');
+          } else {
+               // Clear the previous quotes
+
+               const prevResult = document.querySelector('#result div');
+               if(prevResult != null) {
+                    prevResult.remove();
+               }
+               // Make the quotation
+               const insurance = new Insurance(make,year, level );
+               const price = insurance.calculateQuotation(insurance);
+
+               // Print the result from HTMLUI();
+               html.showResults(price, insurance);
+          }
+     });
 }
 
 
-//Objects
 
-//Quotation and calculation
-function Insurance(make, year, level){
-    this.make = make;
-    this.year = year;
-    this.level = level;
+
+// Objects
+
+
+// Everything related to the quotation and calculations is Insurance
+function Insurance(make, year, level) {
+     this.make = make;
+     this.year = year;
+     this.level = level;
 }
-//Calculate price
-Insurance.prototype.calculateQuotation = function(insurance){
-    let price;
-    const base = 2000;
+// Calculate the price for the current quotation
+Insurance.prototype.calculateQuotation = function(insurance) {
+     let price;
+     const base = 2000;
 
-    //get the make
-    const make = insurance.make;
+     // get the make
+     const make = insurance.make;
 
-    switch(make){
-        case '1':
-                price = base * 1.15;
-                break;
-        case '2':
-                price = base * 1.05;
-                break;
-        case '3':
-                price = base * 1.35;
-                break;
+     /*
+          1 = American 15%
+          2 = Asian 05%
+          3 = European 35%
+     */
+    switch(make) {
+          case '1': 
+               price = base * 1.15;
+               break;
+          case '2': 
+               price = base * 1.05;
+               break;
+          case '3': 
+               price = base * 1.35;
+               break;
     }
 
-    //Get the year
+    // Get the year
     const year = insurance.year;
 
+     // Get the years difference
     const difference = this.getYearDifference(year);
 
-    //Make it cheaper each year
+    // Each year the cost of the insurance is going to be 3% cheaper
     price = price - ((difference * 3) * price) / 100;
-
-    //Check protection level
+    
+    // Check the level of protection
     const level = insurance.level;
 
     price = this.calculateLevel(price, level);
+
+    return price;
+}
+// Returns the difference between years
+Insurance.prototype.getYearDifference = function(year) {
+     return new Date().getFullYear() - year;
+}
+// Adds the value based on the level of protection
+Insurance.prototype.calculateLevel = function(price, level) {
+     /*
+          Basic insurance is going to increase the value by 30%
+          Complete Insurance is going to increaste the value by 50%
+     */
+    if(level === 'basic') {
+         price = price * 1.30;
+    } else {
+           price = price * 1.50;
+    }
+
     return price;
 }
 
-//Return year difference
-Insurance.prototype.getYearDifference = function(year){
-    return new Date().getFullYear() - year;
+// Everything related to the HTML
+function HTMLUI() {}
+
+// Displays the latest 20 years in the select
+HTMLUI.prototype.displayYears = function() {
+     // Max & minimum years
+     const max = new Date().getFullYear(),
+           min = max - 20;
+
+     // Generate the list with the latest 20 years
+     const selectYears = document.getElementById('year');
+
+     // Print the values
+     for(let i = max; i > min; i-- ) {
+          const option = document.createElement('option');
+          option.value = i;
+          option.textContent = i;
+          selectYears.appendChild(option);
+     }
+}
+// Prints an error
+
+HTMLUI.prototype.displayError = function(message) {
+     // create a div
+     const div = document.createElement('div');
+     div.classList = 'error';
+
+     // insert the message
+     div.innerHTML = `
+          <p>${message}</p>
+     `;
+
+     form.insertBefore(div, document.querySelector('.form-group'));
+
+     // Remove the error
+     setTimeout(function() {
+          document.querySelector('.error').remove();
+     }, 3000);
 }
 
-//Calculation level base
-Insurance.prototype.calculateLevel(price, level){
-    if(level === 'basic'){
-        price = price * 1.30;
-    }else{
-        price = price * 1.50;
-    }
-    return price;
+// Prints the result into the HTML
+HTMLUI.prototype.showResults = function(price, insurance) {
+     // Print the result
+     const result = document.getElementById('result');
 
-}
+     // create a div with the result
+     const div = document.createElement('div');
 
-//HTML 
-function HTMLUI(){}
+     // Get Make from the object and assign a readable name
+     let make = insurance.make;
 
-//Display lates 20 years
-HTMLUI.prototype.displayYears = function(){
-    //Max & min years
-    const max = new Date().getFullYear(),
-          min = max - 20;
+     switch(make) {
+          case  '1':
+               make = 'American';
+               break;
+          case  '2':
+               make = 'Asian';
+               break;
+          case  '3':
+               make = 'European';
+               break;
+               
+     }
 
-    //Generate the list with the latest 20 years
-    const selectYears = document.getElementById('year');
 
-    //Print values
-    for(let i = max; i > min; i--){
-        const option = document.createElement('option');
-        option.value = i;
-        option.textContent = i;
-        selectYears.appendChild(option);
-    }
-}
+     // Insert the result
+     div.innerHTML = `
+          <p class="header">Summary</p>
+          <p>Make: ${make}</p>
+          <p>Year: ${insurance.year}</p>
+          <p>Level: ${insurance.level}</p>
+          <p class="total">Total: $ ${price}</p>
+     `;
 
-HTMLUI.prototype.displayError = function(message){
-    //create a div
-    const div = document.createElement('div');
-    div.classList = 'error';
+     const spinner = document.querySelector('#loading img');
+     spinner.style.display = 'block';
 
-    //insert the message
-    div.innerHTML = `
-    <p>${message}</p>
-    `;
+     setTimeout(function() {  
+          spinner.style.display = 'none';
+          // Insert this into the HTML
+          result.appendChild(div);
+     }, 3000);
 
-    form.insertBefore(div, document.querySelector('.form-group'));
 
-    //Remove error
-    setTimeout(function(){
-        document.querySelector('.error').remove();
-    }, 3000);
 }
